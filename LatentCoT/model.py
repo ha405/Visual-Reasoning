aa3e13ff0e0fc9f2829d2e0b14abe7476fe14f56
DASH <haseeb099m@gmail.com>
2025-08-02 01:05:52 +0500

    added directory for cotformer

---
+++ LatentCoT/model.py
@@ -0,0 +1,56 @@
+import torchvision
+import torch.nn as nn
+import timm
+from LatentWrapper import LatentRepeat
+import torch
+
+# [DEBUG]
+# nrepeat = 3
+# model = timm.create_model('vit_small_patch16_224.augreg_in21k', pretrained=True)
+# blocks = model.blocks
+# head = model.head
+# print(head)
+# cls = model.cls_token
+# print(cls)
+# weight = blocks[0].attn.qkv.weight
+# print(weight)
+# unpooled = model.forward_features(torch.randn(2,3,224,224))
+# pooled = model.forward_head(unpooled)
+# print(unpooled.shape)
+# print(pooled.shape)
+# [DEBUG] 
+
+class LatenViTSmall(nn.Module):
+    def __init__(self, model, nrepeat, start,end, num_layers):
+        super().__init__()
+        self.model = model
+        self.nrepeat = nrepeat
+        self.start = start
+        self.end = end
+        self.num_layers = num_layers
+        self.cotformers = self.model.blocks[start:end]
+        self.latent_module = LatentRepeat(self.cotformers, self.nrepeat)
+    
+    def forward(self, x):
+        # x is a batch of images (B, H, E) 
+        x = self.model.patch_embed(x)
+        # cls token is [1,1,D] -> after expansion its [B, 1, D]
+        cls_token = self.model.cls_token.expand(x.shape[0], -1, -1) # expand cls token across all batch of images
+        x = torch.cat((cls_token, x), dim=1)
+        x = self.model.pos_drop(x)
+        x = self.model.patch_drop(x)
+        x = self.model.norm_pre(x)
+        for i in range(self.start):
+            x = self.model.blocks[i](x)
+        x = self.latent_module(x)
+        for i in range(self.end,self.num_layers + 1):
+            x = self.model.blocks[i](x)
+        
+        x = self.model.norm(x)
+        x = x[:, 0]
+        x = self.model.head(x)
+
+        return x 
+  
+
+
