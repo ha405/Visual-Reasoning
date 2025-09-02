6ccedd6d3f0b6161b73532d96a747606523e1b01
Mohammad Fatim Shoaib <26100004@lums.edu.pk>
2025-09-02 09:51:36 +0500

    transferred logics to .py files

---
+++ ModifiedGRPO/configuration.py
@@ -0,0 +1,19 @@
+from imports import *
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Device: {DEVICE}")
+SEED = 42
+BATCH_SIZE = 24
+NUM_EPOCHS = 5
+NUM_CLASSES = 7
+DATA_ROOT = "../../../pacs_data/pacs_data"
+DOMAINS = ["art_painting", "cartoon", "photo", "sketch"]
+MODELS = {
+    "base": "google/vit-base-patch16-224-in21k",
+    "small": "WinKawaks/vit-small-patch16-224",
+    "tiny": "WinKawaks/vit-tiny-patch16-224"
+    }
+
+torch.manual_seed(SEED)
+np.random.seed(SEED)
+random.seed(SEED)
---
+++ ModifiedGRPO/dataset_wrapper.py
@@ -0,0 +1,18 @@
+from configuration import *
+
+class PACSDataset:
+    def __init__(self, data_root, domains, transform):
+        self.data_root = data_root
+        self.domains = domains
+        self.transform = transform
+
+    def get_dataloader(self, domain, train=True):
+        dataset = datasets.ImageFolder(os.path.join(self.data_root, domain), transform=self.transform)
+        
+        indices = list(range(len(dataset)))
+        train_idx, val_idx = train_test_split(indices, test_size=0.2, stratify=[dataset.targets[i] for i in indices], random_state=SEED)
+        selected_idx = train_idx if train else val_idx
+        
+        subset = Subset(dataset, selected_idx)
+        loader = DataLoader(subset, batch_size=BATCH_SIZE, shuffle=train)
+        return loader
